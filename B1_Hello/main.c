7893515ed88e5299f26037f1782d21977d509d3b
Tony Hoang <hoangtrongminh304@gmail.com>
2016-10-24 11:43:08 +0700

    hellp

---
+++ B1_Hello/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello 1 \n");
+
+    int a = 2;
+    printf("Hello a =  %d\n", a);
+
+    printf("Press any key to exit...");
+    getchar();
+    return 0;
+}
